6f4800c482eca0ef690c39ce6556757411be6896
TheRealSlick <wadams0107@gmail.com>
2024-06-02 17:17:20 -0700

    added line that made assignment work, was missing script src for charts

--- public/index.js
+++ public/index.js
@@ -1,58 +1,22 @@
-
-
-
-// const { GME, MSFT, DIS, BNTX } = mockData;
-
-// const stocks = [GME, MSFT, DIS, BNTX];
-
-
-function getColor(stock) {
-    if (stock === "GME") {
-        return 'rgba(61, 161, 61, 0.7)'
-    }
-    if (stock === "MSFT") {
-        return 'rgba(209, 4, 25, 0.7)'
-    }
-    if (stock === "DIS") {
-        return 'rgba(18, 4, 209, 0.7)'
-    }
-    if (stock === "BNTX") {
-        return 'rgba(166, 43, 158, 0.7)'
-    }
-}
-
-function findHigh(values) {
-    let highest = 0;
-    values.forEach(value => {
-        if (parseFloat(value.high) > highest) {
-            highest = value.high
-        }
-    })
-    return highest
-}
-
-function calculateAvg(values) {
-    let total = 0;
-    values.forEach(value => {
-        total += parseFloat(value.high)
-    })
-    return total / values.length
-}
-
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-    const response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=6d7e454931ad424db26320c8a86bae03')
+    const response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=')
     const result = await response.json()
 
-    let GME = result.GME
-    let MSFT = result.MSFT
-    let DIS = result.DIS
-    let BNTX = result.BNTX
+    // let GME = result.GME
+    // let MSFT = result.MSFT
+    // let DIS = result.DIS
+    // let BNTX = result.BNTX
 
-    const stocks = [GME, MSFT, DIS, BNTX]
+    // const stocks = [GME, MSFT, DIS, BNTX]
+
+    const { GME, MSFT, DIS, BNTX } = mockData;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+
 
     stocks.forEach( stock => stock.values.reverse())
 
@@ -109,6 +73,39 @@ async function main() {
                 }]
             }
         });
+    
+        function getColor(stock) {
+            if (stock === "GME") {
+                return 'rgba(61, 161, 61, 0.7)'
+            }
+            if (stock === "MSFT") {
+                return 'rgba(209, 4, 25, 0.7)'
+            }
+            if (stock === "DIS") {
+                return 'rgba(18, 4, 209, 0.7)'
+            }
+            if (stock === "BNTX") {
+                return 'rgba(166, 43, 158, 0.7)'
+            }
+        }
+        
+        function findHigh(values) {
+            let highest = 0;
+            values.forEach(value => {
+                if (parseFloat(value.high) > highest) {
+                    highest = value.high
+                }
+            })
+            return highest
+        }
+        
+        function calculateAvg(values) {
+            let total = 0;
+            values.forEach(value => {
+                total += parseFloat(value.high)
+            })
+            return total / values.length
+        }
     
     
 
